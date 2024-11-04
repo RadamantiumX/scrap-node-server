edb022fad409c623698fa1ee0e23c5af7ff74261
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-11-03 23:14:37 -0300

    adding mongo db connection

---
+++ db/mongo-conn.js
@@ -0,0 +1,13 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from 'dotenv'
+dotenv.config()
+
+const uri = process.env.MONGODB_CONNECT_STRING_URI
+
+export const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
